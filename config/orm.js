d81b49ecf685fead5be4bcb7e8af59b82fc22ac0
unknown <jlbarbut@ncsu.edu>
2018-06-03 20:24:54 -0400

    FE functionality-ish

--- config/orm.js
+++ config/orm.js
@@ -13,6 +13,7 @@ function printQuestionMarks(num){
 function objToSql(ob){
     var arr = [];
     for (var key in ob){
+        var value = ob[key];
         if(Object.hasOwnProperty.call(ob, key)){
             if (typeof value === "string" && value.indexOf(" ")>=0){
                 value = "'" + "'";
--- public/assets/js/FEburger.js
+++ public/assets/js/FEburger.js
@@ -2,8 +2,9 @@ $(function() {
     $(".eat-it").on("click", function(event){
         console.log("hi!");
         var id = $(this).data("id");
+        console.log(id);
 
-        $.ajax("api/burgers/" + id, {
+        $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: {devoured: true}
         }).then(function(){
